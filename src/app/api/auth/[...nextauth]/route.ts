a2e57e7076c5adc0384bd4f5383d5560b9262cdb
Benjo <ben.ohman@gokstadakademiet.no>
2024-02-29 00:20:04 +0100

    Still setting up basics, prisma-github-still not finished

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,17 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import GitHubProvider from "next-auth/providers/github";
+import nextAuth from "next-auth";
+
+export const authOptions = {
+    secret: process.env.NEXTAUTH_SECRET,
+    adapter: PrismaAdapter,
+    providers: [
+        GitHubProvider({
+          clientId: process.env.GITHUB_ID ?? "",
+          clientSecret: process.env.GITHUB_SECRET ?? ""
+        })
+      ]
+}
+
+export const handler = nextAuth(authOptions);
+export {handler as GET,handler as POST};
