edc4330f4f53a22b68cdff18a746662e9debb570
Benjo <ben.ohman@gokstadakademiet.no>
2024-03-02 09:59:00 +0100

    logging-in-page finished and brushed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,16 +8,28 @@ export default function Home() {
   if(session) {
     return(
       <div>
-        <p>Name:{session.user?.name}</p>
+        <p>
+          Name:{session.user?.name}
+        </p>
         <img src={session.user?.image ?? ""} alt="githubpic" className="w-24"/>
-        <button onClick={()=>signOut()}>Sign Out</button>
+        <button onClick={()=>signOut()}>
+          Sign Out
+        </button>
       </div>
     ) 
   }
 
     return (
-      <div>
-        <h1>You are not logged in!</h1><button onClick={()=>signIn("github")}>Sign in here</button>
+      <div className="flex justify-center mt-20">
+        <div className="flex flex-col text-center gap-4">
+            <div className="flex justify-center">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/1245px-Logo_of_Twitter.svg.png" className="w-12" alt="twitter-logo" />
+            </div>
+            <h1 className="text-2xl font-semibold">Happening now</h1>
+            <h2 className="text-xl">Join today</h2>
+            <button className="text-white bg-[#60a5fa] rounded-xl p-2" onClick={()=>signIn("github")}>Sign in with GitHub</button>
+            <p className="text-sm">Do not have a GitHub account yet? Sign up <a href="https://github.com" target="_blank" className="underline">here</a> </p>
+        </div>
       </div>
     );
 }
