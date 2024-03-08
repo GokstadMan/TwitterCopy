91e08f63a988e093c379c9c8d263eeff031dc152
Benjo <ben.ohman@gokstadakademiet.no>
2024-03-08 10:34:48 +0100

    ooops...forgot break in one of the loops

--- src/app/api/get-user-posts/[userId]/route.tsx
+++ src/app/api/get-user-posts/[userId]/route.tsx
@@ -40,7 +40,7 @@ export async function GET (request: NextRequest, { params }: { params: { userId:
                     ...post,
                     requesterHasLiked: true
                 }
-                // break;
+                 break;
             } else {
                 postWithLikeStatus = {
                     ...post,
